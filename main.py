f4376545704e78b916c8d9aef3015d3baeeb80ed
Garv Kumar <91613089+garvboiiiii@users.noreply.github.com>
2025-07-17 23:37:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,8 +61,7 @@ def start(message):
     "📝 Log memories, 🎧 share voice notes, and 🌍 explore thoughts of others anonymously.\n\n"
     "Earn 🌱 by returning daily.\nLet’s grow your garden together!",
     reply_markup=menu_buttons(user_id),
-    parse_mode='Markdown'
-)
+    parse_mode='Markdown')
 
 @bot.message_handler(commands=['help'])
 def help_cmd(message):
